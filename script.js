538b2bc18aec79592d411300d9134d11450b7702
ebergeng <59417456+ebergeng@users.noreply.github.com>
2021-03-16 15:08:55 +0100

    dd

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+function myFunction() {
+    var style = document.createElement('style');
+    style.innerHTML = `
+    #login_inner {
+    transform: rotateY(180deg);
+    }
 
+    .login_back {
+        backface-visibility: hidden;}
+    `;
+    document.head.appendChild(style);
+}
+
+function myFunction1() {
+    var style = document.createElement('style');
+    style.innerHTML = `
+    #login_inner {
+    transform: rotateY(0deg);
+    }
+
+    .login_front .login_wrapper {
+        display: grid;
+    }
+    `;
+    document.head.appendChild(style);
+}
